5ba326242eb73cb7fc65b0fba980aefff83cde9a
Tashakuz <natashafmbinda@gmail.com>
2023-03-16 14:25:58 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Return : 0
+ * Return :Always  0(Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
