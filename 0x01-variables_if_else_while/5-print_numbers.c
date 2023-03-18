8c20821ee86bab6247f2c9a4038fb3e100eed96e
Tashakuz <natashafmbinda@gmail.com>
2023-03-18 15:16:30 -0700

    Update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program to print single digit numbers of base 10
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0;i <= 9;i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
