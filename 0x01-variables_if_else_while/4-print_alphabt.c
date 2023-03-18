781ff2aaf9ab71b04ac2e0479bfc78dab823c0ec
Tashakuz <natashafmbinda@gmail.com>
2023-03-18 15:06:06 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program to print alphabet letters except e and q
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+		if(i != 'e' && i != 'q')
+			putchar(i);
+	putchar('\n');
+	return(0);
+}
