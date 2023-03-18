d87369cfca97e7c7d43eacb0d7f2509d471003a2
Tashakuz <natashafmbinda@gmail.com>
2023-03-18 15:54:41 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program to print single digit numbers
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
