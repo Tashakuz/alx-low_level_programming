cae26cead0a3c142c4ca1132f34a9dea61a25907
Tashakuz <natashafmbinda@gmail.com>
2023-03-18 15:08:28 -0700

    Update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
-		if(i != 'e' && i != 'q')
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' && i != 'q')
 			putchar(i);
 	putchar('\n');
-	return(0);
+	return (0);
 }
