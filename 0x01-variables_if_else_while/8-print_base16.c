2b3cf3a0ba374e8906e8cab3c747840ab0a7d47f
Tashakuz <natashafmbinda@gmail.com>
2023-03-19 01:33:57 -0700

    Update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program to print base 16 numbers
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
