a086f48cde6dc1ef93d2dfd6907a49055f7025a3
Tashakuz <natashafmbinda@gmail.com>
2023-03-19 02:03:48 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * program to print all possible combinations of single digit numbers
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
