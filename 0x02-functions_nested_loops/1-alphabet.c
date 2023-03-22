7b45bf371031a2575234fefbf4066278ddf6f0a7
Tashakuz <natashafmbinda@gmail.com>
2023-03-22 13:22:09 -0700

    to lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * function to print alphabet in lowercase
+ *
+ * Return: 0 Success
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
