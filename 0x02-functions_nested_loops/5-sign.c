664d54af7af61c71299b9e0f7844ccb475084223
Tashakuz <natashafmbinda@gmail.com>
2023-03-22 14:45:28 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - checks whether n is greater than, less than or equal to 0
+ *
+ * return 1 if greater than and -1 if less than
+ *
+ * return 0 if n is equal to zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
