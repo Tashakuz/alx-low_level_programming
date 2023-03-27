4a150315f9d26b423652863c76eff77ab0cc2636
Tashakuz <natashafmbinda@gmail.com>
2023-03-27 15:31:33 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function to return length of string
+ *
+ * @s: string to be input
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		*s++;
+	}
+	return count;
+}
