cf4de001ec345c3de3cdaeb52f7a12238ffc9ea6
Tashakuz <natashafmbinda@gmail.com>
2023-03-31 08:32:34 -0700

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concanate two strings but add inputted number of bytes
+ *
+ * @dest: string to be appended upon
+ * @src: string to be completed
+ * @n: integer parameter to compare index to
+ *
+ * Return: returns new string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
